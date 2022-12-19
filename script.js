40e3e9b5b028b4f7771a7324ab1c060b414562f8
AJ Matise <105606366+AJ-Matise@users.noreply.github.com>
2022-12-18 21:17:50 -0800

    Update script.js
    added vars for the shoe name and shoe image

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Vars:
+var shoeName = 'Jordan 11 Cherry Red';
+var shoeImage = 'https://media.discordapp.net/attachments/973673510016802816/1050221299554865314/920713_01.png?width=511&height=305';
+
+document.getElementById("shoe-name").innerHTML = shoeName;
+document.getElementById("image").src = shoeImage;
+
 function recordData() {
     // Get the input values
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
 
-  
+
     // Print the input values to the console
     console.log("Name: " + name);
     console.log("Email: " + email);
