befa42da068725259c007ad7c13f6c4c55fd77c1
AJ Matise <105606366+AJ-Matise@users.noreply.github.com>
2022-12-18 18:28:18 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function recordData() {
+    // Get the input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+  
+    // Print the input values to the console
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+  }
+  
